e690b2594da8bb70e9aca0a441d80b0f10498a14
Talibshekh <143866271+Talibshekh@users.noreply.github.com>
2023-09-25 22:50:52 -0700

    Add files via upload

---
+++ variable.py
@@ -0,0 +1,32 @@
+print("danish")
+x = 50
+print(x)
+#like x ki pe kuch likh skte jaise naam alphabet
+student = 30
+print(student)
+
+
+#decimal jo value use hum flotting point value or flotting value
+length = 11.5
+print(length)
+
+
+# string string me ek letter paragrph sentence store kr skte hai
+  # variable ko number start nhi kr skte hai lekin text se start kr skte hai
+text = "a"  # letter
+print(text)
+text1 ="danish" # sentence
+print(text1)
+text2 ="this is my fist language" #paragraph
+print(text2)
+
+
+          #polian value
+   #polian value kwal True or False me de skte ho
+   # or cotetion use nhi kr skte hai or true or flase ka pahla letter capital me hona chahiye
+
+text3 = True
+print(text3)
+
+text4 = False
+print(text4)
